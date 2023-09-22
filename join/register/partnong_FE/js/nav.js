6799de5b01ec06c5069778f0a70062698ce4a38d
sanghyeon <sanghyenp9294@gmail.com>
2023-09-22 23:22:21 +0900

    마이페이지 네브 수정

--- join/register/partnong_FE/js/nav.js
+++ join/register/partnong_FE/js/nav.js
@@ -10,3 +10,24 @@ xhr.onreadystatechange = function () {
 
 xhr.open("GET", "nav.html", true);
 xhr.send();
+
+function isUserLoggedIn() {
+    if (localStorage.getItem('token') != null) {
+        return true;
+    }
+    else { return false; }
+}
+
+// 로그인 상태 확인
+var loginCheckElement = document.getElementById("login-check");
+if (loginCheckElement) {
+    if (isUserLoggedIn()) {
+        // 로그인 상태인 경우
+        window.location.href = 'http://localhost:8081/mypage'
+    } else {
+        // 로그인 상태가 아닌 경우
+        loginCheckElement.textContent = "로그인";
+        window.location.href = '로그인 url작성해주세요'
+    }
+}
+
